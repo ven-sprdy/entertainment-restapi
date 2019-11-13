b45dcf0aadcfdfadd141d1c2015b241e2fb544dc
prasad <417.prasad@gmail.com>
2019-11-12 23:00:41 -0600

    Adding some more controllers.

--- src/main/java/com/prasaddy/entertainmentapp/controller/MovieController.java
+++ src/main/java/com/prasaddy/entertainmentapp/controller/MovieController.java
@@ -3,6 +3,9 @@ package com.prasaddy.entertainmentapp.controller;
 import com.prasaddy.entertainmentapp.io.entity.MovieEntity;
 import com.prasaddy.entertainmentapp.service.movie.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -24,14 +27,37 @@ public class MovieController {
     }
 
     @GetMapping("/languages/{movieName}")
-    public @ResponseBody List<MovieEntity> getByMovieName(@PathVariable String movieName, HttpServletResponse response) {
+    public @ResponseBody List<MovieEntity> getByMovieName(@PathVariable String movieName) {
         return movieService.getByMovieName(movieName).stream()
                 .sorted(Comparator.comparing(MovieEntity::getMovieName)).collect(Collectors.toList());
     }
 
     @GetMapping("/{language}")
-    public @ResponseBody List<MovieEntity> getMovieByLanguage(@PathVariable String language, HttpServletResponse response) {
+    public @ResponseBody List<MovieEntity> getMovieByLanguage(@PathVariable String language) {
         return movieService.getMovieByLanguage(language).stream()
                 .sorted(Comparator.comparing(MovieEntity::getMovieName)).collect(Collectors.toList());
     }
+
+    @GetMapping("/")
+    public @ResponseBody List<MovieEntity> getDistinctMovies() {
+        return movieService.getDistinctMovies();
+    }
+
+    @GetMapping("/images")
+    public @ResponseBody List<String> getImageUrlPaths() {
+        return movieService.getImageUrl();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postMovies(@RequestBody List<MovieEntity> movieEntities) {
+        movieService.postMovies(movieEntities);
+        return new ResponseEntity<>("Inserted", HttpStatus.OK);
+    }
+
+    @PutMapping("/{movieId}")
+    public ResponseEntity<String> postMovies(@PathVariable String movieId, @RequestBody MovieEntity movieEntitie) {
+        movieService.updateMovie(movieId, movieEntitie);
+        return new ResponseEntity<>("Updated", HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/prasaddy/entertainmentapp/service/movie/MovieService.java
+++ src/main/java/com/prasaddy/entertainmentapp/service/movie/MovieService.java
@@ -5,7 +5,8 @@ import com.prasaddy.entertainmentapp.io.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
@@ -23,5 +24,39 @@ public class MovieService {
 
     public List<MovieEntity> getMovieByLanguage(String language) {
         return movieRepository.findByLanguageIgnoreCaseContaining(language);
+    }
+
+    public List<MovieEntity> getDistinctMovies() {
+        List<MovieEntity> sortedMovies = movieRepository.findAll().stream().sorted(Comparator.comparing(MovieEntity::getMovieName)).collect(Collectors.toList());
+        List<MovieEntity> movies =  sortedMovies.stream().collect(Collectors.groupingBy(p -> p.getMovieName())).values().stream()
+                .map(plans -> plans.stream().findFirst().get())
+                .collect(Collectors.toList());
+        return movies.stream().sorted(Comparator.comparing(MovieEntity::getMovieName)).collect(Collectors.toList());
+    }
+
+    public void postMovies(List<MovieEntity> movieEntities) {
+        movieEntities.forEach(movie -> {
+            if(movieRepository.findByMovieName(movie.getMovieName()) == null) {
+                movieRepository.save(movie);
+            }
+        });
+    }
+
+    public void updateMovie(String id, MovieEntity movieEntity) {
+        MovieEntity movie = movieRepository.findById(id).get();
+        if(movie != null) {
+            movie.setMovieName(movieEntity.getMovieName());
+            movieRepository.save(movie);
+        }
+    }
+
+    public List<String> getImageUrl() {
+        List<String> imagePaths = new ArrayList<>();
+        movieRepository.findAll().stream().forEach(movieEntity -> {
+            if(movieEntity.getMovieImage() != null) {
+                imagePaths.add(movieEntity.getMovieImage());
+            }
+        });
+        return imagePaths;
     }
 }
